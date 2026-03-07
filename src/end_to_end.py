1aa725b58df6c668a9e7323ea762d4ceb9ec36b6
Ondrej Capek <ocapek@gmail.com>
2026-03-07 18:36:16 +0100

    final not deep testing

--- src/end_to_end.py
+++ src/end_to_end.py
@@ -1,43 +1,35 @@
 from typing import Callable, Tuple, List
 
 from src.bin.excel_in_out import ExcelInOut
+
+
 class Main(ExcelInOut):
     def __init__(self):
         super().__init__()
 
+    def _dry_run(self):
+        if self.p.import_source:
+            self.report_errors()
+        self.get_overview()
+
     def _get_operations(self) -> List[Tuple[bool, Callable]]:
-        """Return list of (condition, operation) tuples."""
         return [
-            (self.p.delete, self._execute_delete),
-            (self.p.import_source, self._execute_import_source),
-            (self.p.join, self._execute_join),
-            # ... etc
+            (self.p.dry_run, self._dry_run),
+            (self.p.delete, self.delete_data),
+            (self.p.import_source, self.import_source),
+            (self.p.join, self.join_data),
+            (self.p.import_backup, self.import_backup_excel),
+            (self.p.export_backup, self.export_backup_excel),
+            (self.p.export_final, self.get_final_content)
         ]
 
     def main(self) -> None:
         self.check_args()
-        if self.p.dry_run:
-            if self.p.import_source:
-                self.report_errors()
-            self.get_overview()
-        else:
-            # for condition, operation in self._get_operations():
-            #     if condition:
-            #         operation()
-
-
-            if self.p.delete:
-                self.delete_data()
-            if self.p.import_source:
-                self.import_source()
-            if self.p.join:
-                self.join_data()
-            if self.p.import_backup:
-                self.import_backup_excel()
-            if self.p.export_backup:
-                self.export_backup_excel()
-            if self.p.export_final:
-                self.get_final_content()
+        for condition, operation in self._get_operations():
+            if condition:
+                    operation()
+                    if operation == self._dry_run:
+                        exit(0)
 
 
 if __name__ == "__main__":
